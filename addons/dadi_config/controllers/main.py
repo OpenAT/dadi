2719eb488657a28399fae5d594016ccb2416b8e0
Michael Karrer <michaelkarrer81@gmail.com>
2016-10-13 16:38:24 +0200

    [UPDATE] o8r36 [IMP] dadi_conf controller information that _REQUIRED field is not neede to be changed

--- addons/dadi_config/controllers/main.py
+++ addons/dadi_config/controllers/main.py
@@ -7,6 +7,8 @@ from openerp.addons.website_crm.controllers.main import contactus
 
 class contactus_dadi(contactus):
     # Remove "company name" from the required fields in the contact form
+    # ATTENTION: This is NOT NEEDED because contact_name is copied to name anyway in the original controller
+    #            Therefore this is ONLY an example on how to change static variables in a class and not needed at all
     @http.route()
     def contactus(self, **kwargs):
         # original Value: ['name', 'contact_name', 'email_from', 'description']
